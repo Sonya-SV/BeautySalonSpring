8362aefb6c672afacf680a2021faa7ada1722cb5
Sonya <sofi.svyrydenko@gmail.com>
2019-11-26 13:46:01 +0200

    add constant messages

--- src/main/java/com/training/salon/controller/ITextConstant.java
+++ src/main/java/com/training/salon/controller/ITextConstant.java
@@ -1,5 +1,14 @@
 package com.training.salon.controller;
 
 public interface ITextConstant {
+    int DAYS_iN_SCHEDULE = 7;
+
     String USER_EXISTS = "User is already exists";
+    String DIFFERENT_PASSWORDS="Passwords are different!";
+    String SAVED_SUCCESSFULLY ="Successfully saved!";
+    String ALREADY_BOOKED = "Already booked";
+    String MASTER_PROCEDURE_DISCREPANCY = "Master doesn't do such procedure";
+    String UNAVAILABLE_TIME = "These date and time unavailable";
+    String UNABLE_TO_CREATE_RECORD = "Unable to create a record";
+
 }
